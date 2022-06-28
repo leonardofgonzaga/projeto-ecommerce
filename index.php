9a5ad18ad51671fc1f91e1efa664fcae4bc2c55a
Leonardo <leonardo.fg95@gmail.com>
2022-06-27 21:42:27 -0300

    Calculate freight

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-require_once("functions.php");
-require_once("site.php");
-require_once("admin.php");
-require_once("admin-users.php");
-require_once("admin-categories.php");
-require_once("admin-products.php");
+require_once("routes/functions.php");
+require_once("routes/site.php");
+require_once("routes/admin.php");
+require_once("routes/admin-users.php");
+require_once("routes/admin-categories.php");
+require_once("routes/admin-products.php");
 
 $app->get("/info", function(){
 
