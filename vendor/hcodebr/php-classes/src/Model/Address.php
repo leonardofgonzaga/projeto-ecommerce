08159672b750bbd9ba7147c7ffe1386f7c086c18
Leonardo <leonardo.fg95@gmail.com>
2022-07-18 22:00:58 -0300

    Boleto

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -36,6 +36,7 @@ class Address extends Model {
 
         if (isset($data['logradouro']) && $data['logradouro']) {
             
+            
             $this->setdesaddress($data['logradouro']);
             $this->setdescomplement($data['complemento']);
             $this->setdesdistrict($data['bairro']);
@@ -55,9 +56,9 @@ class Address extends Model {
         $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
             ':idaddress'=>$this->getidaddress(),
             ':idperson'=>$this->getidperson(),
-            ':desaddress'=>utf8_decode($this->getdesaddress()),
+            ':desaddress'=>$this->getdesaddress(),
             ':desnumber'=>$this->getdesnumber(),
-            ':descomplement'=>utf8_decode($this->getdescomplement()),
+            ':descomplement'=>$this->getdescomplement(),
             ':descity'=>utf8_decode($this->getdescity()),
             ':desstate'=>utf8_decode($this->getdesstate()),
             ':descountry'=>utf8_decode($this->getdescountry()),
